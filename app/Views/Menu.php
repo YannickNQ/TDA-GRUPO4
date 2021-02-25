74fc5d5fe9eb3dbffa3e49934a76911291ae3948
Nick <yannicknq3@gmail.com>
2021-02-25 15:06:28 -0500

    Subiendo archivo .env

--- app/Views/Menu.php
+++ app/Views/Menu.php
@@ -1,13 +1,16 @@
 <body>
-    <nav class="navbar navbar-expand-ml navbar-light">
-        <div class="navbar-nav">
-            <ul >
-                <li class="nav-item"><a href="#" class="nav-link"></a>Inicio</li>
-                <li class="nav-item"><a href="#" class="nav-link"></a>Alumnos</li>
-                <li class="nav-item"><a href="#" class="nav-link"></a>Docentes</li>
-            </ul>
+    <div class="container-fluid">
+        <div class="container-fluid">
+            <nav class="navbar-brand">
+                <div class="navbar-nav">
+                    <ul >
+                        <li class="nav-item"><a href="#" class="nav-link active"></a>Inicio</li>
+                        <li class="nav-item"><a href="#" class="nav-link"></a>Alumnos</li>
+                        <li class="nav-item"><a href="#" class="nav-link"></a>Docentes</li>
+                    </ul>
+                </div>
+            </nav>
         </div>
-    </nav>
-    
+    </div>
 </body>
 </html>
