69228599b7c45c481acf9653712807427a5d4f4e
LuisAnghel <luis@gmail.com>
2021-02-25 00:47:45 -0500

    actualizando bootstrap

--- app/Views/Menu.php
+++ app/Views/Menu.php
@@ -1,13 +1,12 @@
 <body>
-    <nav class="navbar navbar-expand-ml navbar-light">
-        <div class="navbar-nav">
-            <ul >
-                <li class="nav-item"><a href="#" class="nav-link"></a>Inicio</li>
-                <li class="nav-item"><a href="#" class="nav-link"></a>Alumnos</li>
-                <li class="nav-item"><a href="#" class="nav-link"></a>Docentes</li>
+    <nav class="navbar navbar-expand-ml navbar-light bg-light">
+        <div class="container">
+            <ul class="navbar-nav">
+                <li class="nav-item btn btn-primary d-inline"><a href="#" class="nav-link"></a>Inicio</li>
+                <li class="nav-item btn btn-primary d-inline"><a href="#" class="nav-link"></a>Alumnos</li>
+                <li class="nav-item btn btn-primary d-inline"><a href="#" class="nav-link"></a>Docentes</li>
             </ul>
         </div>
     </nav>
     
 </body>
-</html>
