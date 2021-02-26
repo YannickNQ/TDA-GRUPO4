d7ab96dd5e470909862b3d06595e71c511fdb535
LuisAnghel <luis@gmail.com>
2021-02-25 20:10:11 -0500

    subiendo archivos

--- app/Views/Template/Inicio.php
+++ app/Views/Template/Inicio.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 3 | Dashboard</title>
+  <title>TDA-GRUPO4 | Dashboard</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Font Awesome -->
@@ -64,7 +64,7 @@
     <a href="index3.html" class="brand-link">
       <img src="public/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <span class="brand-text font-weight-light">TDA-GRUPO4</span>
     </a>
 
     <!-- Sidebar -->
