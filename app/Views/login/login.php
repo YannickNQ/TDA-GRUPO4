f7b12412b403a27ec0142107d9182c4c803eb298
OMMS <ommamanis@est.unap.edu.pe>
2021-03-02 20:52:16 -0500

    login change-1

--- app/Views/login/login.php
+++ app/Views/login/login.php
@@ -33,23 +33,21 @@
 		<div class="container-login100" style="background-image: url('public/Login/images/bg-01.jpg');">
 			<div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
 				<form class="login100-form validate-form">
-					<span class="login100-form-title p-b-49">
-						Ingrese sus Datos
-					</span>
+					<span class="login100-form-title p-b-49">Iniciar Sesión</span>
 
 					<div class="wrap-input100 validate-input m-b-23" data-validate = "Username is reauired">
-						<span class="label-input100">Username</span>
-						<input class="input100" type="text" name="username" placeholder="ingrese su username">
+						<span class="label-input100">Usuario</span>
+						<input class="input100" type="text" name="username" placeholder="Ingrese su nombre de usuario">
 						<span class="focus-input100" data-symbol="&#xf206;"></span>
 					</div>
 
 					<div class="wrap-input100 validate-input" data-validate="Password is required">
 						<span class="label-input100">Contraseña</span>
-						<input class="input100" type="password" name="pass" placeholder="ingrese su contraseña">
+						<input class="input100" type="password" name="pass" placeholder="Ingrese su contraseña">
 						<span class="focus-input100" data-symbol="&#xf190;"></span>
 					</div>
 					
-					<div class="text-right p-t-8 p-b-31">
+					<div class="text-right p-t-8 p-b-30">
 						<a href="#">
 							¿se olvido su contraseña?
 						</a>
@@ -64,7 +62,7 @@
 						</div>
 					</div>
 
-					<div class="txt1 text-center p-t-54 p-b-20">
+					<!-- <div class="txt1 text-center p-t-54 p-b-20">
 						<span>
 							Puedes registrarte con
 						</span>
@@ -82,9 +80,9 @@
 						<a href="#" class="login100-social-item bg3">
 							<i class="fa fa-google"></i>
 						</a>
-					</div>
+					</div> -->
 
-					<div class="flex-col-c">
+					<div class="flex-col-c p-t-30">
 						<span class="txt1 p-b-17">
 							Registrate aqui
 						</span>
